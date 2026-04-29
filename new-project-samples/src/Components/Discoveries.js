a35e33d3a0f0e413b902fbb2778c79202a57920c
JohnnJhedrickTarronas1 <jhedtarronas@gmail.com>
2026-04-29 22:26:39 +0800

    Update Discoveries.js

--- new-project-samples/src/Components/Discoveries.js
+++ new-project-samples/src/Components/Discoveries.js
@@ -1,5 +1,25 @@
+import React, { use, useState } from "react";
+
 function Discoveries() {
-  return <h1>Discover Amazing Places</h1>;
+  const [search, setSearch]= useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+  };
+  return (
+  <div>
+    <h1>Discover Amazing Places</h1>
+    <form onSubmit={handleSubmit}>
+      <input 
+      type="text"
+      placeholder="Search"
+      value={search}
+      onChange={(e)=> setSearch(e.target.value)}
+      />
+    </form>
+  </div>
+
+  )
 }
 
 export default Discoveries;
