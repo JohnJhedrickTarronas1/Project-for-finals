4f0aed7a061a4100c404e3f02e6019bb1ad6bb5a
JohnnJhedrickTarronas1 <jhedtarronas@gmail.com>
2026-04-29 22:37:19 +0800

    Update Discoveries.js

--- new-project-samples/src/Components/Discoveries.js
+++ new-project-samples/src/Components/Discoveries.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 
 function Discoveries() {
   const [search, setSearch]= useState("");
+  const [error, setError] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!search){
+      setError("No findings");
+      return;
+    }
+    setError("")
     console.log(search);
   };
+  
   return (
   <div>
     <h1>Discover Amazing Places</h1>
@@ -18,6 +26,8 @@ function Discoveries() {
       />
       <button type="submit">Search</button>
     </form>
+
+    {error && <p style={{ color: "red" }}>{error}</p>}
   </div>
 
   )
